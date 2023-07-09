39c504017db435275b58500a50d1efe29d48c1d4
Steff <skaterkeks@googlemail.com>
2023-07-09 23:58:04 +0200

    Implement ContentDialog and more

--- PixeLList/Helpers/ResourceExtensions.cs
+++ PixeLList/Helpers/ResourceExtensions.cs
@@ -8,11 +8,6 @@ namespace PixeLList.Helpers
 {
     internal static class ResourceExtensions
     {
-        private static readonly ResourceLoader _resourceLoader = new();
 
-        public static string GetLocalized(this string resourceKey)
-        {
-            return _resourceLoader.GetString(resourceKey);
-        }
     }
 }
---
+++ PixeLList/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Windows.Storage;
+
+namespace PixeLList.ViewModels
+{
+    [ObservableObject]
+    public partial class MainWindowViewModel
+    {
+        [ObservableProperty]
+        private StorageFolder? _folder;
+
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
